81849d9daff0e92ac04a5cf58c5da3e46d64f730
Dave Hulbert <dave.hulbert@basecreativeagency.com>
2012-06-08 14:22:31 +0100

    WordPress widget

---
+++ widget/basefm.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Base FM
+Plugin URI: http://fm.wearebase.com/
+Description: Base FM
+Author: Base Creative Agency
+Version: 0.1
+Author URI: http://www.basecreativeagency.com
+*/
+
+
