de8d37310f7e965eaba1ad60a626ea4a58984726
Dave Hulbert <dave.hulbert@basecreativeagency.com>
2012-06-08 16:09:47 +0100

    Test API endpoints

--- backend/web/index.php
+++ backend/web/index.php
@@ -2,6 +2,10 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
 $app = new Silex\Application();
 
 $app['debug'] = true;
@@ -11,8 +15,84 @@ $app->get('/', function() {
     return 'Hello!';
 }); 
 
-$app->get('/1/{query}', function($query) { 
-    return 'this is where the api will be';
+$app->get('/1/check', function(Request $request) use ($app) {
+    $since = (int) $request->get('since');
+    $spotify_user_id = $request->get('user');
+
+    // test tracks
+    return $app->json(array(
+        array('id' => 'spotify:track:6JEK0CvvjDjjMUBFoXShNZ'),
+        array('id' => 'spotify:track:5LJN5trTe8v9XRLvqi9SUl'),
+        array('id' => 'spotify:track:5Xz0M0zDEanLfXdAgzOXvJ'),
+        array('id' => 'spotify:track:7sDZGHKFGRI82AwpdpFRrl'),
+        array('id' => 'BROKEN!'),
+        array('id' => 'spotify:track:3KWTRlId9U5SHNh56Ds1gz'),
+    ));
 }); 
+
+
+$app->get('/1/now-playing', function(Request $request) use ($app) {
+    $since = (int) $request->get('since');
+    $spotify_user_id = $request->get('user');
+
+    // test tracks
+    return $app->json(array(
+        'title'   => 'Hakuna Matata',
+        'artist'  => 'Disney',
+        'seconds' => '180',
+    ));
+}); 
+
+$app->get('/1/upcoming', function(Request $request) use ($app) {
+    $since = (int) $request->get('since');
+    $spotify_user_id = $request->get('user');
+
+    // test tracks
+    return $app->json(array(
+        array(
+            'title'   => 'Circle of Life',
+            'artist'  => 'Disney',
+            'seconds' => '150',
+        ),
+        array(
+            'title'   => 'Prodigy',
+            'artist'  => 'Fire Starter',
+            'seconds' => '240',
+        ),
+    ));
+}); 
+
+
+$app->get('/1/history', function(Request $request) use ($app) {
+    $since = (int) $request->get('since');
+    $spotify_user_id = $request->get('user');
+
+    // test tracks
+    return $app->json(array(
+        array(
+            'title'   => 'Circle of Life',
+            'artist'  => 'Disney',
+            'seconds' => '150',
+        ),
+        array(
+            'title'   => 'Prodigy',
+            'artist'  => 'Fire Starter',
+            'seconds' => '240',
+        ),
+    ));
+}); 
+
+
+$app->get('/1/stats', function(Request $request) use ($app) {
+    $since = (int) $request->get('since');
+    $spotify_user_id = $request->get('user');
+
+    // test tracks
+    return $app->json(array(
+        'tweets'   => '9999',
+        'played'  => '5',
+    ));
+}); 
+
 
 $app->run();
