daed73270d3387737a21a70977ee6d5265b17444
Dave Hulbert <dave.hulbert@basecreativeagency.com>
2012-06-08 15:02:15 +0100

    More testy

--- spotify-app/test.js
+++ spotify-app/test.js
@@ -3,7 +3,9 @@ var last_check = null,
     poll_time = 10 * 1000,
     api_url = 'http://fm.wearebase.com/api/1/check';
 
+
 setInterval(function() {
+    var url = api_url + '?&user=' . SPOTIFY.GET_USER() . 'since' = (new Date).getTime();
     $.getJSON(api_url, function(data) {
         for (var i in data)) {
             var track_id  data[i].id;
